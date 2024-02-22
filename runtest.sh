b9c29c69eb639f5ad74d26fea1707bde1e0d4efd
Bastiaan Evers <evers.bastiaan@gmail.com>
2024-02-22 12:10:34 +0100

    Update runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 neuralplexer-inference --task=batched_structure_sampling \
                        --input-receptor /home/bastiaan_evers/3PB4.pdb \
-                       --input-ligand <ligand>.sdf \
+                       --input-ligand /home/bastiaan_evers/pq912.sdf \
                        --use-template  --input-template /home/bastiaan_evers/3BP4.pdb \
                        --out-path /home/bastiaan_evers/results \
                        --model-checkpoint /home/bastiaan_evers/neuralplexermodels_downstream_datasets_predictions/models/complex_structure_prediction.ckpt \
